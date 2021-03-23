a1b243033f4bad3502f9d6371f5ae30071bb984a
awesomeusha <78401938+awesomeusha@users.noreply.github.com>
2021-03-23 11:36:37 +0530

    initial commit

---
+++ popup-block-tab/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CustomizedDialogs from "./components/PopUp";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomizedDialogs />
+    </div>
+  );
+}
+
+export default App;
