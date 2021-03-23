9abb99cce14de0e8df125ebd4a0b61c8d32c512d
awesomeusha <78401938+awesomeusha@users.noreply.github.com>
2021-03-23 11:59:49 +0530

    tabs

--- popup-block-tab/src/App.js
+++ popup-block-tab/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CustomizedDialogs from "./components/PopUp";
+import MyTabs from "./components/Tab";
 
 function App() {
   return (
     <div className="App">
       <CustomizedDialogs />
+      <MyTabs />
     </div>
   );
 }
--- popup-block-tab/src/components/IconTab.js
+++ popup-block-tab/src/components/IconTab.js
@@ -4,9 +4,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import PhoneIcon from "@material-ui/icons/Phone";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import HelpIcon from "@material-ui/icons/Help";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import IconTab from "./IconTab";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -15,8 +19,8 @@ function TabPanel(props) {
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`scrollable-auto-tabpanel-${index}`}
-      aria-labelledby={`scrollable-auto-tab-${index}`}
+      id={`scrollable-prevent-tabpanel-${index}`}
+      aria-labelledby={`scrollable-prevent-tab-${index}`}
       {...other}
     >
       {value === index && (
@@ -24,8 +28,6 @@ function TabPanel(props) {
           <Typography>{children}</Typography>
         </Box>
       )}
-
-      {index === 0 && <IconTab />}
     </div>
   );
 }
@@ -38,20 +40,20 @@ TabPanel.propTypes = {
 
 function a11yProps(index) {
   return {
-    id: `scrollable-auto-tab-${index}`,
-    "aria-controls": `scrollable-auto-tabpanel-${index}`,
+    id: `scrollable-prevent-tab-${index}`,
+    "aria-controls": `scrollable-prevent-tabpanel-${index}`,
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    width: "100%",
+    width: "52.2%",
     backgroundColor: theme.palette.background.paper,
   },
 }));
 
-export default function MyTabs() {
+export default function IconTab() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -61,19 +63,27 @@ export default function MyTabs() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default">
+      <AppBar position="static">
         <Tabs
           value={value}
           onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
           variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs example"
+          scrollButtons="off"
+          aria-label="scrollable prevent tabs example"
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab icon={<PhoneIcon />} aria-label="phone" {...a11yProps(0)} />
+          <Tab
+            icon={<FavoriteIcon />}
+            aria-label="favorite"
+            {...a11yProps(1)}
+          />
+          <Tab icon={<PersonPinIcon />} aria-label="person" {...a11yProps(2)} />
+          <Tab icon={<HelpIcon />} aria-label="help" {...a11yProps(3)} />
+          <Tab
+            icon={<ShoppingBasket />}
+            aria-label="shopping"
+            {...a11yProps(4)}
+          />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
@@ -84,6 +94,12 @@ export default function MyTabs() {
       </TabPanel>
       <TabPanel value={value} index={2}>
         Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
       </TabPanel>
     </div>
   );
