7d1a2a3f95d01ae73573fcc49dcbb4c2a36a26b0
awesomeusha <78401938+awesomeusha@users.noreply.github.com>
2021-03-23 14:04:00 +0530

    getting tabs from data

--- popup-block-tab/src/App.js
+++ popup-block-tab/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import popupData from "./assets/json/popupData.json";
+import tabData from "./assets/json/tabsData.json";
 import CustomPopup from "./components/CustomPopup";
 import Button from "@material-ui/core/Button";
-import CustomizedDialogs from "./components/PopUp";
 import MyTabs from "./components/Tab";
 import MyBlock from "./components/Block";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
 
 function App() {
   const [showDialog, setShowDialog] = React.useState(true);
@@ -20,18 +22,29 @@ function App() {
 
   return (
     <div className="App">
-      <Button variant="outlined" color="primary" onClick={handleDialogOpen}>
-        Open dialog
-      </Button>
-      <CustomPopup
-        doShowDialog={showDialog}
-        handleClose={handleDialogClose}
-        title={popupData.title}
-        subTitle={popupData.subTitle}
-        detail={popupData.detail}
-      />
-      <MyTabs />
-      <MyBlock />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h4">{"Test-1 ->PopUp"}</Typography>
+          <Button variant="outlined" color="primary" onClick={handleDialogOpen}>
+            Open dialog
+          </Button>
+        </Grid>
+        <CustomPopup
+          doShowDialog={showDialog}
+          handleClose={handleDialogClose}
+          title={popupData.title}
+          subTitle={popupData.subTitle}
+          detail={popupData.detail}
+        />
+        <Grid item xs={12}>
+          <Typography variant="h4">{"Test-2 ->Tabs"}</Typography>
+          <MyTabs myTabData={tabData} />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4">{"Test-3 ->Block"}</Typography>
+          <MyBlock />
+        </Grid>
+      </Grid>
     </div>
   );
 }
